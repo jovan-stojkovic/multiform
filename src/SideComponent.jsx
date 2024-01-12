112a4cdd2886e305e2faccfa8a264c5b47a6a025
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-01-12 07:40:42 +0100

    side logic

--- src/SideComponent.jsx
+++ src/SideComponent.jsx
@@ -1,7 +1,51 @@
+import { NavLink } from "react-router-dom";
+
 const SideComponent = () => {
-    return ( <>
-    hello from the left side
-    </> );
-}
- 
+  return (
+    <div className="side">
+      <div className="side-container">
+        <div className="single-link">
+          <NavLink to={"/"}>
+            <p className="link-number">1</p>
+          </NavLink>
+          <div className="single-link-info">
+            <p className="step-count">STEP 1</p>
+            <p className="step-desc">YOUR INFO</p>
+          </div>
+        </div>
+
+        <div className="single-link">
+          <NavLink to={"/second"}>
+            <p className="link-number">2</p>
+          </NavLink>
+          <div className="single-link-info">
+            <p className="step-count">STEP 2</p>
+            <p className="step-desc">SELECT PLAN</p>
+          </div>
+        </div>
+
+        <div className="single-link">
+          <NavLink to={"/third"}>
+            <p className="link-number">3</p>
+          </NavLink>
+          <div className="single-link-info">
+            <p className="step-count">STEP 3</p>
+            <p className="step-desc">ADD-ONS</p>
+          </div>
+        </div>
+
+        <div className="single-link">
+          <NavLink to={"/fourth"}>
+            <p className="link-number">4</p>
+          </NavLink>
+          <div className="single-link-info">
+            <p className="step-count">STEP 4</p>
+            <p className="step-desc">SUMMARY</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default SideComponent;
