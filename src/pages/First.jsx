8c11153f0209e65bbc71178f66281fb662f8eb50
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-01-12 07:51:45 +0100

    first page content

--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -1,7 +1,47 @@
+import { Link } from "react-router-dom";
+
 const First = () => {
     return ( <>
-    This is the first page
-    </> );
+        <div className="page-container">
+          <div className="headline">
+            <h1>Personal info</h1>
+            <p className="headline-desc">
+              Please provide your name, email address, and phone number.
+            </p>
+          </div>
+  
+          <div className="form">
+            <div className="form-text">
+              <h5>Name</h5>
+              <h6>This field is required</h6>
+            </div>
+            <input type="text" placeholder="e.g. Stephen King" />
+          </div>
+  
+          <div className="form">
+            <div className="form-text">
+              <h5>Email Address</h5>
+              <h6>This field is required</h6>
+            </div>
+            <input type="text" placeholder="e.g. stephenking@lorem.com" />
+          </div>
+  
+          <div className="form">
+            <div className="form-text">
+              <h5>Phone Number</h5>
+              <h6>This field is required</h6>
+            </div>
+            <input type="text" placeholder="e.g. +1 234 567 890" />
+          </div>
+
+          
+          <div className="buttons first-page-buttons">
+            <Link className="next-button" to="/second">
+              Next Step
+            </Link>
+          </div>
+        </div>
+      </> );
 }
  
 export default First;
--- src/pages/Second.jsx
+++ src/pages/Second.jsx
@@ -1,7 +1,45 @@
 const Second = () => {
-    return ( <>
-    This is the second page
-    </> );
+  return ( <>
+    <div className="page-container">
+      <div className="headline">
+        <h1>Select your plan</h1>
+        <p className="headline-desc">
+        You have the option of monthly or yearly billing.
+        </p>
+      </div>
+
+      <div className="form">
+        <div className="form-text">
+          <h5>Name</h5>
+          <h6>This field is required</h6>
+        </div>
+        <input type="text" placeholder="e.g. Stephen King" />
+      </div>
+
+      <div className="form">
+        <div className="form-text">
+          <h5>Email Address</h5>
+          <h6>This field is required</h6>
+        </div>
+        <input type="text" placeholder="e.g. stephenking@lorem.com" />
+      </div>
+
+      <div className="form">
+        <div className="form-text">
+          <h5>Phone Number</h5>
+          <h6>This field is required</h6>
+        </div>
+        <input type="text" placeholder="e.g. +1 234 567 890" />
+      </div>
+
+      
+      <div className="buttons first-page-buttons">
+        <Link className="next-button" to="/second">
+          Next Step
+        </Link>
+      </div>
+    </div>
+  </> );
 }
  
 export default Second;
